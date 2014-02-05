c8ea5838cc5712ac6a4df5cd355bba14553a8803
PyMarcos <marcos_costa.sjc@hotmail.com>
2014-02-05 21:44:26 -0200

    Criado o template base da aplicação stock

--- stock/registrations.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf8 -*-
-__author__ = 'Marcos'
-
-from stock.models import *
-from stock.admin.admin_models import *
-from django.contrib import admin
-
-admin.site.register(Product, ProductAdmin)
